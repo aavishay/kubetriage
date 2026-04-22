4a82e78e13df87292a35f8c2078195d82a54b84e
Avishay Ashkenazi <aavishay@gmail.com>
2026-04-22 22:51:53 +0300

    chore: Bump Homebrew formula and rebuild assets for v1.5.1

--- homebrew/kubetriage.rb
+++ homebrew/kubetriage.rb
@@ -2,27 +2,27 @@ class Kubetriage < Formula
   desc "KubeTriage - Autonomous SRE Guard for Kubernetes"
   homepage "https://github.com/aavishay/kubetriage"
   license "MIT"
-  version "1.5.0"
+  version "1.5.1"
 
   on_macos do
     on_arm do
       url "https://github.com/aavishay/kubetriage/releases/download/v#{version}/kubetriage-v#{version}-darwin-arm64.tar.gz"
-      sha256 "312b3989f37ff1d290b04ff8c7b22e34fd26b4f3ee79deaf04b96e78832ec044"
+      sha256 "7c6da00302b2c383f66b6d724a6efc0d4ff398f7119c473e1740c7a91e1d1b1f"
     end
     on_intel do
       url "https://github.com/aavishay/kubetriage/releases/download/v#{version}/kubetriage-v#{version}-darwin-amd64.tar.gz"
-      sha256 "0b497210f742b155d52efb0390511cf1005c6c85ebff1801100977e63d359cfa"
+      sha256 "c38eedc048a7a7da139dff83df292219e7745cad7bf982075191d40ae083f60b"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/aavishay/kubetriage/releases/download/v#{version}/kubetriage-v#{version}-linux-amd64.tar.gz"
-      sha256 "a688f644a7cc989f3aac979cb37803b25079fec33f7002d4aeb874da43397540"
+      sha256 "de200b69fc824306420dbc2de2b5b0591e05c888448d77d0a689fe0346a6d418"
     end
     on_arm do
       url "https://github.com/aavishay/kubetriage/releases/download/v#{version}/kubetriage-v#{version}-linux-arm64.tar.gz"
-      sha256 "b1a6d2867398597645612b9584d0848d10049d1b32d47ad4d059e38c251b0afd"
+      sha256 "f1236160a4951edc11940aef382e9bfa7eda496e964b16ef857866e5bb791a4a"
     end
   end
 
