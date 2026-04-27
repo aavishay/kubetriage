9b949aefbcce7b0e62d63bc156288345e6d80653
Avishay Ashkenazi <aavishay@gmail.com>
2026-04-27 16:05:18 +0300

    chore: Update Homebrew formula for v1.7.3 with SHA256 hashes

--- homebrew/kubetriage.rb
+++ homebrew/kubetriage.rb
@@ -1,28 +1,28 @@
 class Kubetriage < Formula
   desc "Autonomous SRE Guard for Kubernetes"
   homepage "https://github.com/aavishay/kubetriage"
-  version "1.7.2"
+  version "1.7.3"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/aavishay/kubetriage/releases/download/v1.7.2/kubetriage-v1.7.2-darwin-arm64.tar.gz"
-      sha256 "b8763302d4465c4337fa0f3cf8b0f6e5462cca12918dcf193621d2fb0463bb0c"
+      url "https://github.com/aavishay/kubetriage/releases/download/v1.7.3/kubetriage-v1.7.3-darwin-arm64.tar.gz"
+      sha256 "4c2866bae4c1cc0c3700c2bd166dbe4277e6085376d266c529c22bf6e5fc90b0"
     end
     on_intel do
-      url "https://github.com/aavishay/kubetriage/releases/download/v1.7.2/kubetriage-v1.7.2-darwin-amd64.tar.gz"
-      sha256 "d8d4f586e51162af7026cf3408904e5071d5b91b2eacfcfe25830480ec7d3528"
+      url "https://github.com/aavishay/kubetriage/releases/download/v1.7.3/kubetriage-v1.7.3-darwin-amd64.tar.gz"
+      sha256 "a79ef727402f680215a1de66e0de0d012937f5e9e83deedf30a64e12e5f192d4"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/aavishay/kubetriage/releases/download/v1.7.2/kubetriage-v1.7.2-linux-amd64.tar.gz"
-      sha256 "c1efd07738608b5cd8fd137254b95311788c60fef5bedf9f3ff2fc5c7aa0a2fb"
+      url "https://github.com/aavishay/kubetriage/releases/download/v1.7.3/kubetriage-v1.7.3-linux-amd64.tar.gz"
+      sha256 "0dc448efb0b7ee13ce16760d7ce493e398997e478f1092865341d59806f4f159"
     end
     on_arm do
-      url "https://github.com/aavishay/kubetriage/releases/download/v1.7.2/kubetriage-v1.7.2-linux-arm64.tar.gz"
-      sha256 "ce9da88cd7b7f8e21e9cc4dff375fe73c4ee6b72d3b98e4973ef4a0c7c60fe23"
+      url "https://github.com/aavishay/kubetriage/releases/download/v1.7.3/kubetriage-v1.7.3-linux-arm64.tar.gz"
+      sha256 "fa7486a5ac1271fed606651aac2877702663653163f646de7828a4031c2dae22"
     end
   end
 
